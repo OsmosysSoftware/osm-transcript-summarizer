b8b317e42cdde76eb347538acd584f256d0dc4cb
Hitesh <hitesh.k@osmosys.co>
2024-05-01 11:18:35 +0530

    feat: add logger service and error handler

---
+++ apps/portal/src/app/shared/enum.ts
@@ -0,0 +1,13 @@
+export enum LogLevel {
+  Info = 'INFO',
+  Warn = 'WARN',
+  Error = 'ERROR',
+  Fatal = 'FATAL',
+}
+
+export enum LogSeverity {
+  Critical = 'CRITICAL',
+  High = 'HIGH',
+  Medium = 'MEDIUM',
+  Low = 'LOW',
+}
