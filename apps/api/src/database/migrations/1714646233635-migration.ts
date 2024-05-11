82df851ea236265ee881984282080b856294f96a
ramraut-osm <ram.raut@osmosys.co>
2024-05-11 11:05:49 +0530

    Fixes for migration tablename

--- apps/api/src/database/migrations/1714646233635-migration.ts
+++ apps/api/src/database/migrations/1714646233635-migration.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, Table, TableForeignKey, } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Migration1714646233635 implements MigrationInterface {
 
