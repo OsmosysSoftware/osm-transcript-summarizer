485ab06bf0b095c22f02907ddba8e572b70af19a
Hitesh <hitesh.k@osmosys.co>
2024-05-12 19:53:09 +0530

    chore: add graphql queries and service

--- apps/portal/src/app/graphql/graphql.service.ts
+++ apps/portal/src/app/graphql/graphql.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
+import { DocumentNode } from 'graphql';
+import { QueryRef, Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { ApolloQueryResult } from '@apollo/client';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GraphqlService {}
+export class GraphqlService {
+  constructor(private apollo: Apollo) {}
+
+  query<T>(query: DocumentNode): Observable<ApolloQueryResult<T>> {
+    const queryRef: QueryRef<T> = this.apollo.use('default').watchQuery({
+      query,
+    });
+
+    return queryRef.valueChanges;
+  }
+}
