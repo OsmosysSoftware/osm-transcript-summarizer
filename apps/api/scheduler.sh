c5a1010da63eaaca21eb08bc06f31a60a53de4ca
kshitij-k-osmosys <kshitij.k@osmosys.co>
2024-12-02 21:43:53 +0530

    fix: remove unused script variable

--- apps/api/scheduler.sh
+++ apps/api/scheduler.sh
@@ -10,8 +10,6 @@ add_pending_summaries_to_queue() {
   curl -X POST "${BASE_URL}/schedule" -H "Content-Type: application/json" -d '{}'
 }
 
-last_archive_run=$(date +%s)
-
 while true; do
   add_pending_summaries_to_queue
   sleep $SCHEDULE_TIME
